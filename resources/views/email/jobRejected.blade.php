0aec9ee0965e6c0cb6d5a32404e0d2a4753a2972
Hasnain Abid Khanzada <haniabidkz@gmail.com>
2025-05-28 04:36:52 +0500

    Reject Notfication Text Changes

--- resources/views/email/jobRejected.blade.php
+++ resources/views/email/jobRejected.blade.php
@@ -1,7 +1,11 @@
 <!-- English -->
 <h3>Regret Notification</h3>
-<p>Your job advertisement <strong>{{ $job->title }}</strong> could not be published.</p>
+<p>We regret to inform you that your job advertisement <strong>{{ $job->title }}</strong> could not be published as it does not comply with our policies or company guidelines. </p>
 
 <!-- German -->
 <h3>Ablehnungsmitteilung</h3>
-<p>Ihre Stellenanzeige <strong>{{ $job->title }}</strong> konnte nicht veröffentlicht werden.</p>
+<p> Wir bedauern, Ihnen mitteilen zu müssen, dass Ihre Stellenanzeige <strong>{{ $job->title }}</strong> nicht veröffentlicht 
+werden konnte, da sie nicht mit unseren Richtlinien übereinstimmt.</p>
+
+
+
